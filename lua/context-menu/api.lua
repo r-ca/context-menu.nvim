0fd535f59b13b39b78d93499ee5f95906fb12b20
rca <nem@nem.x0.to>
2024-09-17 11:42:13 +0900

    make outline

---
+++ lua/context-menu/api.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+local function apply_filter(items, context)
+  -- body
+end
+
+local function apply_sort(items, context)
+  -- body
+end
+
+function M.prepare_items(menu_items, context)
+
+end
+
+function M.trigger_context_menu()
+  -- body
+end
+
+function M.menu_popup_window(menu_items, context, local_level)
+    -- body
+end
+
+function M.close_menu(context)
+    -- body
+end
+
+return M
